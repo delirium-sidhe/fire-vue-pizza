d1071281d802538fcb0900c98265a5dc7227b148
Dmitrii Moskalenko <shadowofmadness@gmail.com>
2019-03-05 15:32:35 +0300

    add menu firebase sync

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { firebaseMutations } from 'vuexfire'
 
 import menu from './modules/menu'
 import orders from './modules/orders'
@@ -11,6 +12,7 @@ import users from './modules/users'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  mutations: { ...firebaseMutations },
   modules: {
     menu,
     orders,
